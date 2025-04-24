236c436aad67e5993db7ec3a6db7feec82dced29
Jeffrey Vaughn <75887112+jvaughn1230@users.noreply.github.com>
2025-04-24 05:57:29 -0700

    Update and rename temp.js to vercel.config.js

---
+++ vercel.config.js
@@ -0,0 +1,13 @@
+{
+  "version": 2,
+   Vercel.json
+{
+  "rewrites": [
+    {
+      "source": "/api/graphql",
+      "destination": "/api/graphql"
+    }
+  ],
+  "routes": [{ "src": "/api/graphql", "dest": "/api/graphql" }]
+}
+}
